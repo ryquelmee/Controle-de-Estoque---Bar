37911204f045b6227e0bcdc4d87c7dd9663e5b18
Lucas <lucashasfj2006@gmail.com>
2025-10-25 19:38:07 -0300

    feat(adicionar espaco): Permitir a criação de espaços

--- app/estoque.py
+++ app/estoque.py
@@ -1,11 +1,46 @@
+from collections import deque
+import questionary
+
 class Espaco:
-    def __init__(self, nome, caminho):
+    def __init__(self, nome, caminho = None):
         self.nome = nome
         self.caminho = caminho
-        self.espacos = []
+        self.subespacos = []
 
 
 class Estoque:
-    def __init__(self, raiz = None):
+    def __init__(self, raiz):
         self.raiz = raiz
+
+    def percorrer_estoque(self):
+        fila = deque([self.raiz])
+
+        while fila:
+            espaco_atual = fila.popleft()
+
+            print(espaco_atual.caminho)
+
+            for subespaco in espaco_atual.subespacos:
+                fila.append(subespaco)
+
+
+    def adicionar_espaco(self, nome):
+        fila = deque([self.raiz])
+        while fila:
+            
+            espaco_atual = fila.popleft()
+            subespacos_atuais = [subespaco for subespaco in espaco_atual.subespacos]
+
+            espaco_escolhido = questionary.select("Selecione o espaço: ", choices=["Local atual"] + [subespaco.nome for subespaco in subespacos_atuais]).ask()
+            caminho = (espaco_atual.caminho or "") + "/" + nome
+            if espaco_escolhido == "Local atual":
+                espaco_atual.subespacos.append(Espaco(nome, caminho))
+                return
+            else:
+                for subespaco in espaco_atual.subespacos:
+                    if subespaco.nome == espaco_escolhido:
+                        fila.append(subespaco)
+                        caminho = subespaco.caminho
+                        break
+estoque = Estoque(Espaco("Raiz", ""))
         
